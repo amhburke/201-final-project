c05e73cc3fae846c489783c4d1921e28dce5b4e3
Iliana <ilianamorgan7@gmail.com>
2025-11-22 15:53:52 -0500

    added api keys and urls to the call api function

---
+++ project code.py
@@ -0,0 +1,33 @@
+#Names: Iliana Chevres and Avery Burke 
+
+def call_apis():
+    country_api_key = "https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY"
+    news_api_key = "552f2cf7b2c444ca872c26be4c389a0d"
+    
+    country_api_url = " https://restcountries.com/v3.1/all"
+    news_api_url = 'https://newsapi.org/v2/top-headlines'
+    
+    response_country = ""
+    response_news = ""
+    pass 
+def get_top_headlines():
+    pass 
+def get_country_status():
+    pass 
+def store_headlines():
+    pass 
+def store_country_data():
+    pass 
+def count_headlines_by_month():
+    pass 
+def calculate_relationship_status():
+    pass 
+def join_headline_and_country_data():
+    pass 
+def create_scatter_plot():
+    pass 
+def create_boxplot():
+    pass 
+
+def main():
+    pass 
