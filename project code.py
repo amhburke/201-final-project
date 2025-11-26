6aa8431a4f453b79ab2f1b9260c0757f9a54ca9a
Iliana <ilianamorgan7@gmail.com>
2025-11-25 20:30:15 -0500

    started the get top headlines function

--- project code.py
+++ project code.py
@@ -3,18 +3,24 @@ import requests
 import json
 import sqlite3
 
-def call_apis():
+def call_apis(country):
     country_api_key = "https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY"
     news_api_key = "552f2cf7b2c444ca872c26be4c389a0d"
     
     country_api_url = " https://restcountries.com/v3.1/all"
     news_api_url = 'https://newsapi.org/v2/top-headlines'
     
-    response_country = requests.get()
-    response_news = requests.get()
+    response_country = requests.get(country_api_url)
+    response_news = requests.get(news_api_url, params={"country":country, "apiKey": news_api_key})
+    
+    return response_country.json(), response_news.json()
+def get_top_headlines(country):
+    call_apis(country)
+    data = response_country.json()
+    headlines = []
+    for item in data:
+        if 
 
-def get_top_headlines():
-    pass 
 def get_country_status():
     pass 
 def store_headlines():
@@ -33,4 +39,8 @@ def create_boxplot():
     pass 
 
 def main():
-    pass 
+    pass
+
+main()
+
+#test cases 
