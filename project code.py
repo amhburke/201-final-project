7ac035c56b33af898ecfcc0464335b3c6d49b325
Iliana <ilianamorgan7@gmail.com>
2025-12-01 16:29:58 -0500

    started creating the sql database and headlines table

--- project code.py
+++ project code.py
@@ -5,7 +5,7 @@ import sqlite3
 import matplotlib
 
 def call_apis(country):
-    country_api_key = "https://newsapi.org/v2/top-headlines?country=&apiKey=API_KEY"
+    #country_api_key = "https://newsapi.org/v2/top-headlines?country=&apiKey=API_KEY"
     news_api_key = "552f2cf7b2c444ca872c26be4c389a0d"
 
     #have to use different urls for the country api to access the different stuff?
@@ -28,12 +28,21 @@ def get_headlines(country):
                 headlines.append(article['title'])
     return headlines 
 
-print(get_headlines("US"))
-
 def get_country_status():
     pass 
-def store_headlines():
-    pass 
+
+def store_headlines(country):
+    headlines = get_headlines(country)
+    conn = sqlite3.connect('countrynews.db')
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS headlines (
+                )
+                ''')
+    conn.commit()
+    conn.close()
+    
+    print("Database 'countrynews.db'' and 'headlines' table created.") 
 def store_country_data():
     pass 
 def count_headlines_by_month():
